96f2b6cde6ae9560c94b7e432399fe99b6863d9f
mortasebastian <mr.mchaker@gmail.com>
2021-05-25 20:17:11 +0200

    init

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+import pandas as pd
+import numpy as np
+import sklearn
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from sklearn.ensemble import BaseEnsemble
+from sklearn.base import ClassifierMixin, clone
+from sklearn.utils.validation import check_array, check_is_fitted, check_X_y
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class RandomSubspaceEnsemble(BaseEnsemble, ClassifierMixin):
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def __init__(self, base_estimator=None, n_estimators=10, n_subspace_features=5, hard_voting=True,
+                 random_state=None):
+        # Klasyfikator bazowy
+        self.base_estimator = base_estimator
+        # Liczba klasyfikatorow
+        self.n_estimators = n_estimators
+        # Liczba cech w jednej podprzestrzeni
+        self.n_subspace_features = n_subspace_features
+        # Tryb podejmowania decyzji
+        self.hard_voting = hard_voting
+        # Ustawianie ziarna losowosci
+        self.random_state = random_state
+        np.random.seed(self.random_state)
+
+
+class BaggingEnsemble(BaseEnsemble, ClassifierMixin):
+
+    def __init__(self, base_estimator=None, n_estimators=10, hard_voting=True):
+        print('h')
+
