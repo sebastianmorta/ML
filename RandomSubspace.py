feaa440dfeb8800748b0e2daa70adac5129ab570
mortasebastian <mr.mchaker@gmail.com>
2021-05-26 23:38:21 +0200

    init

--- RandomSubspace.py
+++ RandomSubspace.py
@@ -10,32 +10,71 @@ class RandomSubspaceEnsemble(BaseEnsemble, ClassifierMixin):
 
     def __init__(self, base_estimator=None, n_estimators=10, n_subspace_features=5, hard_voting=True,
                  random_state=None):
-        self.base_estimator = base_estimator  # Klasyfikator bazowy
-
-        self.n_estimators = n_estimators  # Liczba klasyfikatorow
-
-        self.n_subspace_features = n_subspace_features  # Liczba cech w jednej podprzestrzeni
-
-        self.hard_voting = hard_voting  # Tryb podejmowania decyzji
-
-        self.random_state = random_state  # Ustawianie ziarna losowosci
+        # Klasyfikator bazowy
+        self.base_estimator = base_estimator
+        # Liczba klasyfikatorow
+        self.n_estimators = n_estimators
+        # Liczba cech w jednej podprzestrzeni
+        self.n_subspace_features = n_subspace_features
+        # Tryb podejmowania decyzji
+        self.hard_voting = hard_voting
+        # Ustawianie ziarna losowosci
+        self.random_state = random_state
         np.random.seed(self.random_state)
 
     def fit(self, X, y):
-        X, y = check_X_y(X, y)  # Sprawdzenie czy X i y maja wlasciwy ksztalt
+        X, y = check_X_y(X, y)
+        self.classes_ = np.unique(y)
+        self.n_features = np.unique(y)
+        self.n_features = X.shape[1]
 
-        self.classes_ = np.unique(y)  # Przehowywanie nazw klas
+        if self.n_subspace_features > self.n_features:
+            raise ValueError("Number")
 
-        self.n_features = X.shape[1]  # Zapis liczby atrybutow
+        self.subspaces = np.random.randint(0, self.n_features, (self.n_estimators, self.n_subspace_features))
 
-        if self.n_subspace_features > self.n_features:  # Czy liczba cech w podprzestrzeni jest mniejsza od calkowitej liczby cech
-            raise ValueError("Number of features in subspace higher than number of features.")
-
-        self.subspaces = np.random.randint(0, self.n_features, (
-        self.n_estimators, self.n_subspace_features))  # Wylosowanie podprzestrzeni cech
-
-        self.ensemble_ = []  # Wyuczenie nowych modeli i stworzenie zespolu
+        # Wyuczenie nowych modeli i stworzenie zespolu
+        self.ensemble_ = []
         for i in range(self.n_estimators):
             self.ensemble_.append(clone(self.base_estimator).fit(X[:, self.subspaces[i]], y))
 
         return self
+
+    def predict(self, X):
+        # Sprawdzenie czy modele sa wyuczone
+        check_is_fitted(self, "classes_")
+        # Sprawdzenie poprawnosci danych
+        X = check_array(X)
+        # Sprawdzenie czy liczba cech się zgadza
+        if X.shape[1] != self.n_features:
+            raise ValueError("number of features does not match")
+
+        if self.hard_voting:
+            # Podejmowanie decyzji na podstawie twardego glosowania
+            pred_ = []
+            # Modele w zespole dokonuja predykcji
+            for i, member_clf in enumerate(self.ensemble_):
+                pred_.append(member_clf.predict(X[:, self.subspaces[i]]))
+            # Zamiana na miacierz numpy (ndarray)
+            pred_ = np.array(pred_)
+            # Liczenie glosow
+            prediction = np.apply_along_axis(lambda x: np.argmax(np.bincount(x)), axis=1, arr=pred_.T)
+            # Zwrocenie predykcji calego zespolu
+            return self.classes_[prediction]
+
+        else:
+            # Podejmowanie decyzji na podstawie wektorow wsparcia
+            esm = self.ensemble_support_matrix(X)
+            # Wyliczenie sredniej wartosci wsparcia
+            average_support = np.mean(esm, axis=0)
+            # Wskazanie etykiet
+            prediction = np.argmax(average_support, axis=1)
+            # Zwrocenie predykcji calego zespolu
+            return self.classes_[prediction]
+
+    def ensemble_support_matrix(self, X):
+        # Wyliczenie macierzy wsparcia
+        probas_ = []
+        for i, member_clf in enumerate(self.ensemble_):
+            probas_.append(member_clf.predict_proba(X[:, self.subspaces[i]]))
+        return np.array(probas_)
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sklearn.utils.validation import check_array, check_is_fitted, check_X_y
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import BaggingClassifier
 
+from RandomSubspace import RandomSubspaceEnsemble
 from scipy.io.arff import loadarff
 
 from sklearn.naive_bayes import GaussianNB
@@ -18,82 +19,83 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import RepeatedStratifiedKFold
 
 number_of_estimators = 50
-estimator = MultinomialNB()
+# estimator = MultinomialNB()
+estimator = GaussianNB()
 dataset = datasets.load_wine()
 
 
-class RandomSubspaceEnsemble(BaseEnsemble, ClassifierMixin):
-
-    def __init__(self, base_estimator=None, n_estimators=10, n_subspace_features=5, hard_voting=True,
-                 random_state=None):
-        # Klasyfikator bazowy
-        self.base_estimator = base_estimator
-        # Liczba klasyfikatorow
-        self.n_estimators = n_estimators
-        # Liczba cech w jednej podprzestrzeni
-        self.n_subspace_features = n_subspace_features
-        # Tryb podejmowania decyzji
-        self.hard_voting = hard_voting
-        # Ustawianie ziarna losowosci
-        self.random_state = random_state
-        np.random.seed(self.random_state)
-
-    def fit(self, X, y):
-        X, y = check_X_y(X, y)
-        self.classes_ = np.unique(y)
-        self.n_features = np.unique(y)
-        self.n_features = X.shape[1]
-
-        if self.n_subspace_features > self.n_features:
-            raise ValueError("Number")
-
-        self.subspaces = np.random.randint(0, self.n_features, (self.n_estimators, self.n_subspace_features))
-
-        # Wyuczenie nowych modeli i stworzenie zespolu
-        self.ensemble_ = []
-        for i in range(self.n_estimators):
-            self.ensemble_.append(clone(self.base_estimator).fit(X[:, self.subspaces[i]], y))
-
-        return self
-
-    def predict(self, X):
-        # Sprawdzenie czy modele sa wyuczone
-        check_is_fitted(self, "classes_")
-        # Sprawdzenie poprawnosci danych
-        X = check_array(X)
-        # Sprawdzenie czy liczba cech się zgadza
-        if X.shape[1] != self.n_features:
-            raise ValueError("number of features does not match")
-
-        if self.hard_voting:
-            # Podejmowanie decyzji na podstawie twardego glosowania
-            pred_ = []
-            # Modele w zespole dokonuja predykcji
-            for i, member_clf in enumerate(self.ensemble_):
-                pred_.append(member_clf.predict(X[:, self.subspaces[i]]))
-            # Zamiana na miacierz numpy (ndarray)
-            pred_ = np.array(pred_)
-            # Liczenie glosow
-            prediction = np.apply_along_axis(lambda x: np.argmax(np.bincount(x)), axis=1, arr=pred_.T)
-            # Zwrocenie predykcji calego zespolu
-            return self.classes_[prediction]
-
-        else:
-            # Podejmowanie decyzji na podstawie wektorow wsparcia
-            esm = self.ensemble_support_matrix(X)
-            # Wyliczenie sredniej wartosci wsparcia
-            average_support = np.mean(esm, axis=0)
-            # Wskazanie etykiet
-            prediction = np.argmax(average_support, axis=1)
-            # Zwrocenie predykcji calego zespolu
-            return self.classes_[prediction]
-
-    def ensemble_support_matrix(self, X):
-        # Wyliczenie macierzy wsparcia
-        probas_ = []
-        for i, member_clf in enumerate(self.ensemble_):
-            probas_.append(member_clf.predict_proba(X[:, self.subspaces[i]]))
-        return np.array(probas_)
+# class RandomSubspaceEnsemble(BaseEnsemble, ClassifierMixin):
+#
+#     def __init__(self, base_estimator=None, n_estimators=10, n_subspace_features=5, hard_voting=True,
+#                  random_state=None):
+#         # Klasyfikator bazowy
+#         self.base_estimator = base_estimator
+#         # Liczba klasyfikatorow
+#         self.n_estimators = n_estimators
+#         # Liczba cech w jednej podprzestrzeni
+#         self.n_subspace_features = n_subspace_features
+#         # Tryb podejmowania decyzji
+#         self.hard_voting = hard_voting
+#         # Ustawianie ziarna losowosci
+#         self.random_state = random_state
+#         np.random.seed(self.random_state)
+#
+#     def fit(self, X, y):
+#         X, y = check_X_y(X, y)
+#         self.classes_ = np.unique(y)
+#         self.n_features = np.unique(y)
+#         self.n_features = X.shape[1]
+#
+#         if self.n_subspace_features > self.n_features:
+#             raise ValueError("Number")
+#
+#         self.subspaces = np.random.randint(0, self.n_features, (self.n_estimators, self.n_subspace_features))
+#
+#         # Wyuczenie nowych modeli i stworzenie zespolu
+#         self.ensemble_ = []
+#         for i in range(self.n_estimators):
+#             self.ensemble_.append(clone(self.base_estimator).fit(X[:, self.subspaces[i]], y))
+#
+#         return self
+#
+#     def predict(self, X):
+#         # Sprawdzenie czy modele sa wyuczone
+#         check_is_fitted(self, "classes_")
+#         # Sprawdzenie poprawnosci danych
+#         X = check_array(X)
+#         # Sprawdzenie czy liczba cech się zgadza
+#         if X.shape[1] != self.n_features:
+#             raise ValueError("number of features does not match")
+#
+#         if self.hard_voting:
+#             # Podejmowanie decyzji na podstawie twardego glosowania
+#             pred_ = []
+#             # Modele w zespole dokonuja predykcji
+#             for i, member_clf in enumerate(self.ensemble_):
+#                 pred_.append(member_clf.predict(X[:, self.subspaces[i]]))
+#             # Zamiana na miacierz numpy (ndarray)
+#             pred_ = np.array(pred_)
+#             # Liczenie glosow
+#             prediction = np.apply_along_axis(lambda x: np.argmax(np.bincount(x)), axis=1, arr=pred_.T)
+#             # Zwrocenie predykcji calego zespolu
+#             return self.classes_[prediction]
+#
+#         else:
+#             # Podejmowanie decyzji na podstawie wektorow wsparcia
+#             esm = self.ensemble_support_matrix(X)
+#             # Wyliczenie sredniej wartosci wsparcia
+#             average_support = np.mean(esm, axis=0)
+#             # Wskazanie etykiet
+#             prediction = np.argmax(average_support, axis=1)
+#             # Zwrocenie predykcji calego zespolu
+#             return self.classes_[prediction]
+#
+#     def ensemble_support_matrix(self, X):
+#         # Wyliczenie macierzy wsparcia
+#         probas_ = []
+#         for i, member_clf in enumerate(self.ensemble_):
+#             probas_.append(member_clf.predict_proba(X[:, self.subspaces[i]]))
+#         return np.array(probas_)
 
 
 # dat_file = r"D:\Python\UM\iris1.dat"
@@ -101,7 +103,22 @@ class RandomSubspaceEnsemble(BaseEnsemble, ClassifierMixin):
 # with open(dat_file, 'r') as file:
 #     text = file.read()
 #     print(text)
+def dataSetReader():
+    data = []
+    with open('datasets\\iris.dat') as file:
+        n, m = file.readline().split()
+        for i in file:
+            if not i.isspace():
+                data.append([int(x) for x in i.split()])
+    return data, int(n), int(m)
 
+
+
+
+
+
+
+dataSetReader()
 X = dataset.data
 y = dataset.target
 print("Total number of features is", X.shape[1])
