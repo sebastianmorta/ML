76407521ffb6b986e0a5174105d1b995865d580e
sebastianmorta <mr.mchaker@gmail.com>
2021-05-27 20:19:45 +0200

    add datasets

--- main.py
+++ main.py
@@ -28,97 +28,31 @@ clfs = {
     'MNB': MultinomialNB()
 }
 estimator = GaussianNB()
+
+
 dataset = datasets.load_wine()
 
 
-# class RandomSubspaceEnsemble(BaseEnsemble, ClassifierMixin):
-#
-#     def __init__(self, base_estimator=None, n_estimators=10, n_subspace_features=5, hard_voting=True,
-#                  random_state=None):
-#         # Klasyfikator bazowy
-#         self.base_estimator = base_estimator
-#         # Liczba klasyfikatorow
-#         self.n_estimators = n_estimators
-#         # Liczba cech w jednej podprzestrzeni
-#         self.n_subspace_features = n_subspace_features
-#         # Tryb podejmowania decyzji
-#         self.hard_voting = hard_voting
-#         # Ustawianie ziarna losowosci
-#         self.random_state = random_state
-#         np.random.seed(self.random_state)
-#
-#     def fit(self, X, y):
-#         X, y = check_X_y(X, y)
-#         self.classes_ = np.unique(y)
-#         self.n_features = np.unique(y)
-#         self.n_features = X.shape[1]
-#
-#         if self.n_subspace_features > self.n_features:
-#             raise ValueError("Number")
-#
-#         self.subspaces = np.random.randint(0, self.n_features, (self.n_estimators, self.n_subspace_features))
-#
-#         # Wyuczenie nowych modeli i stworzenie zespolu
-#         self.ensemble_ = []
-#         for i in range(self.n_estimators):
-#             self.ensemble_.append(clone(self.base_estimator).fit(X[:, self.subspaces[i]], y))
-#
-#         return self
-#
-#     def predict(self, X):
-#         # Sprawdzenie czy modele sa wyuczone
-#         check_is_fitted(self, "classes_")
-#         # Sprawdzenie poprawnosci danych
-#         X = check_array(X)
-#         # Sprawdzenie czy liczba cech się zgadza
-#         if X.shape[1] != self.n_features:
-#             raise ValueError("number of features does not match")
-#
-#         if self.hard_voting:
-#             # Podejmowanie decyzji na podstawie twardego glosowania
-#             pred_ = []
-#             # Modele w zespole dokonuja predykcji
-#             for i, member_clf in enumerate(self.ensemble_):
-#                 pred_.append(member_clf.predict(X[:, self.subspaces[i]]))
-#             # Zamiana na miacierz numpy (ndarray)
-#             pred_ = np.array(pred_)
-#             # Liczenie glosow
-#             prediction = np.apply_along_axis(lambda x: np.argmax(np.bincount(x)), axis=1, arr=pred_.T)
-#             # Zwrocenie predykcji calego zespolu
-#             return self.classes_[prediction]
-#
-#         else:
-#             # Podejmowanie decyzji na podstawie wektorow wsparcia
-#             esm = self.ensemble_support_matrix(X)
-#             # Wyliczenie sredniej wartosci wsparcia
-#             average_support = np.mean(esm, axis=0)
-#             # Wskazanie etykiet
-#             prediction = np.argmax(average_support, axis=1)
-#             # Zwrocenie predykcji calego zespolu
-#             return self.classes_[prediction]
-#
-#     def ensemble_support_matrix(self, X):
-#         # Wyliczenie macierzy wsparcia
-#         probas_ = []
-#         for i, member_clf in enumerate(self.ensemble_):
-#             probas_.append(member_clf.predict_proba(X[:, self.subspaces[i]]))
-#         return np.array(probas_)
-
-
-# dat_file = r"D:\Python\UM\iris1.dat"
-# text = ''
-# with open(dat_file, 'r') as file:
-#     text = file.read()
-#     print(text)
-def dataSetReader(dataset):
-    dataset = np.genfromtxt("%s.csv" % (dataset), delimiter=",")
+def dataSetReader(file):
+    dataset=file
+    dataset = np.genfromtxt("%s.csv" % (file), delimiter=",")
     X = dataset[:, :-1]
     Y = dataset[:, -1].astype(int)
+    print(dataset)
+    return X, Y
 
 
-dataSetReader('datasets/iris')
+# dataSetReader()
 X = dataset.data
 y = dataset.target
+print(X)
+print('-----')
+print(y)
+print('=======')
+X, y = dataSetReader('datasets/iris')
+print(X)
+print('-----')
+print(y)
 print("Total number of features is", X.shape[1])
 
 n_splits = 5
